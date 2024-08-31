d87ec708bc2bab0544563aee845fa9bf69782a86
mikkotenni <mikko.tenni@hotmail.com>
2024-08-31 10:18:46 +0300

    docs: App JSDoc

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import './App.css'
 import Converter from './components/converter/Converter'
 
+/**
+ * App component that renders the Converter component with chosen
+ * units.
+ * @returns The App component.
+ */
 function App() {
   return (
     <Converter unitFrom="centimeter" unitTo="millimeter" />
